50c715853159c2fcab5ddbb1f5a3aa36df22f871
Chalini11 <chalini.pasupathy@m2pfintech.com>
2023-03-30 14:57:55 +0530

    Req2- Add. Added logic and testcase for meter and kilometer units

--- src/main/java/com/m2pfintech/Centimeter.java
+++ src/main/java/com/m2pfintech/Centimeter.java
@@ -11,15 +11,45 @@ public class Centimeter
 
     public double getMagnitude()
     {
-        return this.magnitude;
+        return magnitude;
     }
 
     @Override
     public boolean equals(Object object)
     {
+
+      if(this == object)
+          return true;
+      if(object.getClass() != this.getClass())
+            return false;
+
       Centimeter unit = (Centimeter) object;
-      if(this.magnitude == unit.getMagnitude())
+      if(magnitude == unit.getMagnitude())
           return true;
       return false;
+
+      // that how is it working ...-> its private right.
     }
+    public static Centimeter Kilometer(double kilometer)
+    {
+        return new Centimeter((kilometer*100000));
+    }
+    public static Centimeter Meter(double meter)
+    {
+        return new Centimeter((meter*100));
+    }
+
+//    public double convertToKilometer()
+//    {
+//        return magnitude/100000;
+//    }
+//
+//    public double convertToMeter()
+//    {
+//        return magnitude/100;
+//    }
+
+
+
 }
+
--- src/test/java/com/m2pfintech/MeasurementTest.java
+++ src/test/java/com/m2pfintech/MeasurementTest.java
@@ -2,20 +2,53 @@ package com.m2pfintech;
 
 import org.junit.jupiter.api.Test;
 
+import static com.m2pfintech.Centimeter.Kilometer;
+import static com.m2pfintech.Centimeter.Meter;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 
 public class MeasurementTest
 {
     //
     Centimeter oneCentimeter = new Centimeter(1);
-    Centimeter anotherOneCentimeter = new Centimeter(1);
+    Centimeter oneMeter = Meter(1);
+
+
     @Test
 
     void toReturnTrueIfOneCentimeterIsOneCentimeter()
     {
 
+        Centimeter anotherOneCentimeter = new Centimeter(1);
+
         assertThat(oneCentimeter,is(equalTo(anotherOneCentimeter)));
+    }
+    @Test
+    void toReturnTrueIfOneCentimeterIsNotTneCentimeter()
+    {
+
+        Centimeter anotherOneCentimeter = new Centimeter(2);
+
+        assertThat(oneCentimeter,is(not(equalTo(anotherOneCentimeter))));
+    }
+
+    @Test
+    void toReturnTrueWhenHundredCentimeterIsOneMeter()
+    {
+        Centimeter hundredCentimeter = new Centimeter(100);
+
+        assertThat(oneMeter,is(equalTo(hundredCentimeter)));
+    }
+
+
+    @Test
+    void toReturnTrueWhenHundredCentimeterIsOnePointFiveKilometer()
+    {
+        Centimeter hundredCentimeter = new Centimeter(100);
+        Centimeter kilometer = Kilometer(0.1);
+
+        assertThat(kilometer,is(not(equalTo(hundredCentimeter))));
     }
 }
