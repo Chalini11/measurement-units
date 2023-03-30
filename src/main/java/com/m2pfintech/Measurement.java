3894fba0a11054d2c454a85bff479e1c47f28a3a
Chalini11 <chalini.pasupathy@m2pfintech.com>
2023-03-30 12:01:51 +0530

    Req1- Add. Implemented logic and testcase for centimeter equality check

--- src/main/java/com/m2pfintech/Measurement.java
+++ src/main/java/com/m2pfintech/Measurement.java
@@ -3,4 +3,19 @@ package com.m2pfintech;
 public class Measurement
 {
 
+    int firstValue;
+    int secondValue;
+
+    Measurement(int firstValue, int secondValue)
+    {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+    boolean checkCentimeterCentimeter()
+    {
+        if( firstValue == secondValue)
+            return true;
+        return false;
+    }
 }
--- src/test/java/com/m2pfintech/MeasurementTest.java
+++ src/test/java/com/m2pfintech/MeasurementTest.java
@@ -1,6 +1,22 @@
 package com.m2pfintech;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class MeasurementTest
 {
-    Measurement measurementObject = new Measurement();
+    Measurement measurementObject1cm1cm = new Measurement(1,1);
+
+    @Test
+
+    void toReturnTrueIfOneCentimeterIsOneCentimeter()
+    {
+
+        boolean actualValue = measurementObject1cm1cm.checkCentimeterCentimeter();
+
+        assertThat(actualValue,is(equalTo(true)));
+    }
 }
